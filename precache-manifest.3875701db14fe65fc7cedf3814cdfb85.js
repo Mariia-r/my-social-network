2690ff934592804350819b9c330450306468e98d
Mariia-r <mariya-r1@ukr.net>
2019-10-09 20:03:59 +0300

    for deploy

---
+++ precache-manifest.3875701db14fe65fc7cedf3814cdfb85.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8f3fba300c177aecc754ad5adccb15",
+    "url": "/my-social-network/index.html"
+  },
+  {
+    "revision": "ee494ec2031e2df8355f",
+    "url": "/my-social-network/static/css/3.b4e2322e.chunk.css"
+  },
+  {
+    "revision": "e8d59ff1d561e3386e7b",
+    "url": "/my-social-network/static/css/main.02519161.chunk.css"
+  },
+  {
+    "revision": "5935fc5291dff2e04b37",
+    "url": "/my-social-network/static/js/2.3a03eaf2.chunk.js"
+  },
+  {
+    "revision": "ee494ec2031e2df8355f",
+    "url": "/my-social-network/static/js/3.ebff7ba0.chunk.js"
+  },
+  {
+    "revision": "e8d59ff1d561e3386e7b",
+    "url": "/my-social-network/static/js/main.5de02556.chunk.js"
+  },
+  {
+    "revision": "b960ee9246ec0271a9e0",
+    "url": "/my-social-network/static/js/runtime~main.d9f01f5b.js"
+  },
+  {
+    "revision": "1b4c32b7c6485724b7c923523bd38ad1",
+    "url": "/my-social-network/static/media/lookingForAJob.1b4c32b7.jpg"
+  },
+  {
+    "revision": "71743697969d2e4a2c1b31c67cf68e64",
+    "url": "/my-social-network/static/media/notLookingForAJob.71743697.png"
+  },
+  {
+    "revision": "d263d9fb694410b6d5bf41e57a8c51fe",
+    "url": "/my-social-network/static/media/preloader.d263d9fb.svg"
+  },
+  {
+    "revision": "139885546ce6572b4b6b8712c143afd0",
+    "url": "/my-social-network/static/media/user.13988554.jpg"
+  }
+]);
